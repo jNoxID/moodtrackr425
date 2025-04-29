928704214cf6c7bae6c19c511b094e659d41ec21
Jerome Pointreau <neothecode@outlook.com>
2025-04-29 14:07:56 +0200

    ajout début du backend sauvegarde avant test et déploiement

---
+++ backend/models/Mood.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const MoodSchema = new mongoose.Schema({
+  mood: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Mood', MoodSchema);
